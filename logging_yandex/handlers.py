baea3385742415fce2839d641988a5b41763cd97
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-07-09 18:53:16 +0300

    feat: make two threaded logger (main + logger thread)

--- logging_yandex/handlers.py
+++ logging_yandex/handlers.py
@@ -1,12 +1,9 @@
 import logging
 import sys
 import threading
-from concurrent.futures.process import ProcessPoolExecutor
-from concurrent.futures.thread import ThreadPoolExecutor
-from queue import Queue
-from time import time
 from logging import LogRecord
-from typing import Type
+from time import time
+from typing import Type, Iterable
 
 import yandexcloud
 from google.protobuf import timestamp_pb2
@@ -15,7 +12,6 @@ from yandex.cloud.logging.v1.log_ingestion_service_pb2 import WriteRequest
 from yandex.cloud.logging.v1.log_ingestion_service_pb2_grpc import LogIngestionServiceStub
 from yandex.cloud.logging.v1.log_resource_pb2 import LogEntryResource
 from yandexcloud._sdk import Client
-
 
 TRACE = 60
 
@@ -38,57 +34,22 @@ def get_curr_timestamp() -> Type[timestamp_pb2.Timestamp]:
 
 
 class Emitter:
-    def __init__(self, client: Client, destination: Destination, resource: LogEntryResource, log_batch_size: int = 10, commit_period: int = 10):
+    def __init__(self, client: Client, logs_buffer: Iterable[IncomingLogEntry]):
         self.client = client
-        self.destination = destination
-        self.resource = resource
-        self.log_batch_size = log_batch_size
-
-        self.logs_buffer = []
-        self.commit_period = commit_period
-        self.last_commit = time()
-
-    @property
-    def period_passed(self):
-        return time() >= self.last_commit + self.commit_period
-
-    def write(self):
-        self.client.Write()
+        self.logs_buffer = logs_buffer
 
     def empty_buffer(self):
         self.logs_buffer = []
 
-    def set_commit_time(self):
-        self.last_commit = time()
-
-    def __call__(self, entry: IncomingLogEntry):
-        self.client.Write(self.logs_buffer)
-
-
-class ThreadManager:
-    def __init__(self, max_workers: int = 1):
-        self.element_queue = Queue()
-
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            _ = [executor.submit(self.process_element, queue=self.element_queue) for _ in range(max_workers)]
-
-    @staticmethod
-    def process_element(*, queue: Queue = None):
-        emitter = Emitter()
-        while True:
-            print(f"thread {threading.current_thread()}")
-            value = queue.get()
-            if isinstance(value, IncomingLogEntry):
-                emitter(value)
-            else:
-                break
+    def __call__(self, request: WriteRequest):
+        # TODO: handle errors in this or caller's thread
+        self.client.Write(request)
+        self.empty_buffer()
 
 
 class YandexCloudHandler(logging.Handler):
-
     client: Client | None = None
 
-    # todo make client instance field
     def __new__(cls, credentials: dict[str, str], **kwargs):
         if cls.client is None:
             cls.client = yandexcloud.SDK(**credentials).client(LogIngestionServiceStub)
@@ -117,17 +78,37 @@ class YandexCloudHandler(logging.Handler):
         self.logs_buffer = []
         self.last_commit = time()
 
-        self.emitter = Emitter(self.client, self.destination, self.resource, self.log_batch_size, self.commit_period)
+        self.send = Emitter(
+            client=self.client,
+            logs_buffer=self.logs_buffer,
+            log_batch_size=log_batch_size,
+            commit_period=commit_period
+        )
 
     def emit(self, record: LogRecord):
         try:
             new_entry = self.build_payload(record)
             self.logs_buffer.append(new_entry)
 
-            self.emitter(new_entry)
+            if len(self.logs_buffer) > self.log_batch_size or self.period_passed:
+                write_req = self.build_request()
+                th = threading.Thread(target=self.send, kwargs={"request": write_req}, daemon=True)
+                th.start()
+
+                self.set_commit_time()
+
         except Exception:
             record.exc_info = sys.exc_info()
             self.handleError(record)
+
+    def handleError(self, record):
+        logs = [str(log) + "\n" for log in self.logs_buffer]
+        sys.stdout.writelines(logs)
+
+        super().handleError(record)
+
+    def set_commit_time(self):
+        self.last_commit = time()
 
     def build_payload(self, record: LogRecord) -> IncomingLogEntry:
         formatted_msg = self.format(record)
@@ -135,15 +116,14 @@ class YandexCloudHandler(logging.Handler):
         level = _MAPPED_LOG_LEVELS[record.levelno]
         extra = record.__dict__.get("json_payload", None)
 
-        log_entry = IncomingLogEntry(timestamp=timestamp, level=level, message=formatted_msg, json_payload=extra)
+        log_entry = IncomingLogEntry(
+            timestamp=timestamp,
+            level=level,
+            message=formatted_msg,
+            json_payload=extra,
+        )
 
         return log_entry
-
-    def handleError(self, record):
-        logs = [str(log) + "\n" for log in self.logs_buffer]
-        sys.stdout.writelines(logs)
-
-        super().handleError(record)
 
     def build_request(self) -> WriteRequest:
         write_request = WriteRequest(
@@ -154,8 +134,8 @@ class YandexCloudHandler(logging.Handler):
 
         return write_request
 
-    def empty_buffer(self):
-        self.logs_buffer = []
-
+    @property
+    def period_passed(self):
+        return time() >= self.last_commit + self.commit_period
 
 
